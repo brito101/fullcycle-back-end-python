d14cd6b2a34130824a02d05348d357341b733c49
brito101 <rodrigobrito101@hotmail.com>
2025-07-13 10:15:14 -0300

    feat: add api layer

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for django_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,7 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from django_project.category_app.views import CategoryViewSet
+
+router = DefaultRouter()
+router.register(r"api/categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + router.urls
